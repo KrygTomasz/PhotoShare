9b56b47a03ca22cab72523475716a12f96485542
Krygu <>
2018-11-20 06:53:00 +0100

    Initial commit

---
+++ PhotoShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoShare
+//
+//  Created by Krygu on 20/11/2018.
+//  Copyright © 2018 Krygu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
